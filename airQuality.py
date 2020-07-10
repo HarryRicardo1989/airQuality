17e7a7b7e124267962e1b285316e08da9719bef5
Ricardo Paschoali <ricardo.paschoali@gmial.com>
2020-07-09 21:25:33 -0300

    bugfix

--- airQuality.py
+++ airQuality.py
@@ -2,16 +2,11 @@
 from time import sleep, time
 import datetime as dt
 from insertDb import InsertDB
-from carrega_display import CarregaDisplay
-from read_ccs import ReadCcs
 from read_hdc1080 import ReadHdc1080
 
-display = CarregaDisplay(i2c_bus=1, address=0x27, numLinhas=4)
 insertdb = InsertDB()
 ultima_atualizacao = 0,
 last_update = time()
-ccs = None
-i = 0
 
 
 def should_update(last_update, rate):
@@ -26,24 +21,13 @@ def salva_banco(TEMPERATURA, UMIDADE):
         return
     DATA = dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     insertdb.InsertDB(DATA, TEMPERATURA, UMIDADE)
-    # constr()
     last_update = time()
 
 
-def verifica_horario():
-    if dt.datetime.now().time() > dt.time(18, 0, 0):
-        display.desliga_display()
-    elif dt.datetime.now().time() > dt.time(8, 0, 0):
-        display.liga_display()
-
-
 if __name__ == '__main__':
-    hdc1080 = ReadHdc1080(offsetTemp=-3.5)
+    hdc1080 = ReadHdc1080(offsetTemp=-1.5)
     while(1):
         temperatura = hdc1080.read_temp()
         umidade = hdc1080.read_humid()
-        display.display_line_0("Temperature: %3.2fC" % temperatura)
-        display.display_line_1("Humidity: %3.2f%%" % umidade)
         salva_banco(temperatura, umidade)
-        verifica_horario()
         sleep(0.3)
