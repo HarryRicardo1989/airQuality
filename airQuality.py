6ff4d26b5a1621b2d055c9a19f2566bb79a60e53
Ricardo Paschoali <ricardopaschoali@gmail.com>
2020-09-08 18:05:15 -0300

    include co2 and tvoc

--- airQuality.py
+++ airQuality.py
@@ -1,13 +1,23 @@
 #! /usr/bin/python3
 import datetime as dt
 import os
+import serial
+import math
+from read_ccs import ReadCcs
 from bme280 import *
 from post_Db import POSTDB
 from time import sleep, time
 
+read_ccs811 = ReadCcs()
+uart = serial.Serial('/dev/ttyS0', 115200)
 post_db = POSTDB()
 ultima_atualizacao = 0
-db_update = time()
+db_update = timer_wind_speed = timerccs811 = time()
+media_vel = 0
+media_tvoc = 0
+media_co2 = 0
+n = 1
+n2 = 1
 
 
 def should_update(last_update, rate):
@@ -27,7 +37,52 @@ def salva_banco(temperatura_ar=0, temperatura_orvalho=0, umidade=0, pressao_loca
     db_update = time()
 
 
+def wind_speed():
+    global timer_wind_speed
+    global media_vel
+    global n
+
+    if not should_update(timer_wind_speed, 1):
+        return
+
+    try:
+        raw_value = int(uart.readline().decode())
+        velocidade = (raw_value * math.pi * 0.09)
+        media_vel += 1/n*(velocidade-media_vel)
+        n += 1
+    except Exception as ex:
+        print(ex)
+        sleep(0.1)
+
+    if n > 20:
+        n = 1
+
+
+def ccs811():
+    global timerccs811
+    global media_tvoc
+    global media_co2
+    global n2
+
+    if not should_update(timerccs811, 1):
+        return
+
+    try:
+
+        raw_co2, raw_tvoc = read_ccs811.read_ccs()
+        media_co2 += 1/n*(raw_co2-media_co2)
+        media_tvoc += 1/n*(raw_tvoc-media_tvoc)
+        n2 += 1
+    except Exception as ex:
+        print(ex)
+        sleep(0.1)
+
+    if n2 > 20:
+        n2 = 1
+
+
 if __name__ == '__main__':
+
     while(1):
         barometer = BME280(t_mode=BME280_OSAMPLE_8,
                            p_mode=BME280_OSAMPLE_8, h_mode=BME280_OSAMPLE_8)
@@ -38,9 +93,11 @@ if __name__ == '__main__':
         altitude = barometer.read_altitude()
         pressao_nivel_mar = barometer.read_sealevel_pressure(543)
         temperatura_orvalho = barometer.read_dewpoint()
+        wind_speed()
+        ccs811()
 
         #print(altitude, pressure, sealevel_pressure, temperatura, umidade)
         salva_banco(temperatura_ar=temperatura_ar, temperatura_orvalho=temperatura_orvalho, umidade=umidade,
-                    pressao_local=pressao_local, pressao_nivel_mar=pressao_nivel_mar, altitude=altitude)
+                    pressao_local=pressao_local, pressao_nivel_mar=pressao_nivel_mar, altitude=altitude, wind_speed=media_vel, co2=media_co2, tvoc=media_tvoc)
 
-        sleep(0.3)
+        sleep(0.2)
--- teste.py
+++ teste.py
@@ -1,9 +1,4 @@
-import math
+from read_ccs import ReadCcs
 
-sealevel_pa = 1013.25
-pressure = 947
-T = 29.36
 
-h1 = (10*(-28.93*(1*(T+273))*(math.log(pressure/sealevel_pa))))/10
-
-print(h1)
+print(ReadCcs().read_ccs())
