912f0a35b8d7e6d53fe26a4518837208079cdc50
Ricardo Paschoali <ricardopaschoali@gmail.com>
2020-09-18 09:15:46 -0300

    new DB

--- dbAccount.py
+++ dbAccount.py
@@ -1,5 +1,5 @@
 
-user = "ANDROMEDA"
-password = "andromeda1"
-host = "10.8.0.150"
-database = "air_quality"
+user = "PCD_data"
+password = "PCD#_insert_request_data"
+host = "127.0.0.1"
+database = "pcd_db"
